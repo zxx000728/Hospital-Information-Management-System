f418f6acda7dcfb10df53d3919d921bedfdf2d44
123 <1400208597@qq.com>
2020-12-31 21:13:42 +0800

    create project with vue & springboot

---
+++ hims-backend/src/main/java/com/hims/HimsApplication.java
@@ -0,0 +1,13 @@
+package com.hims;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HimsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HimsApplication.class, args);
+	}
+
+}
---
+++ hims-backend/src/test/java/com/hims/HimsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hims;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HimsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
