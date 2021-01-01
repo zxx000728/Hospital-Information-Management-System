b880ccb5e723fe61ea6e3f400f3664a8c16c9a31
123 <1400208597@qq.com>
2021-01-01 13:34:04 +0800

    add HelloController

---
+++ hims-backend/src/main/java/com/hims/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.hims.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello, this is a Springboot demo!";
+    }
+}
