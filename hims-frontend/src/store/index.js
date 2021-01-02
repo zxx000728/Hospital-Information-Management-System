7d3d7a6d2a2e157da1420f57056c4746caf2ee06
Summer-Shen <1563407190@qq.com>
2021-01-02 22:39:18 +0800

    add vuex store for user state support

---
+++ hims-frontend/src/store/index.js
@@ -0,0 +1,21 @@
+import Vue from "vue";
+import Vuex from "vuex";
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+    user: localStorage.getItem("user") || null
+  },
+  mutations: {
+    login(state, user) {
+      localStorage.setItem("user", user);
+      state.user = user;
+    },
+    logout(state) {
+      // 移除token
+      localStorage.removeItem("user");
+      state.user = null;
+    }
+  },
+  actions: {}
+});
