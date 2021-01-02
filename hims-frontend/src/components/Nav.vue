733c2a535e81a94dabf24529bbd3083727cc7221
Summer-Shen <1563407190@qq.com>
2021-01-02 23:50:30 +0800

    update navmenu style

--- hims-frontend/src/components/Nav.vue
+++ hims-frontend/src/components/Nav.vue
@@ -1,10 +1,10 @@
 <template>
   <el-menu mode="horizontal">
-    <el-menu-item index="0">医院信息管理系统</el-menu-item>
+    <el-menu-item index="0"><strong>医院信息管理系统</strong></el-menu-item>
     <el-menu-item index="1">数据面板</el-menu-item>
     <el-menu-item index="2">消息中心</el-menu-item>
-    <el-menu-item index="3">我的</el-menu-item>
-    <el-menu-item index="4">登出</el-menu-item>
+    <el-menu-item index="4" style="float: right">登出</el-menu-item>
+    <el-menu-item index="3" style="float: right">我的</el-menu-item>
   </el-menu>
 </template>
 
